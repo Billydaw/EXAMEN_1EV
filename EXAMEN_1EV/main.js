af4ad2c5783e80b60c40bbcbfbb02742989a06ee
Billydaw <57388644+Billydaw@users.noreply.github.com>
2020-11-20 10:39:59 +0100

    Add files via upload
    EXAMEN_1EV_Letra

--- EXAMEN_1EV/main.js
+++ EXAMEN_1EV/main.js
@@ -1,1 +1,7 @@
-document.createElement('button');
+let letra = document.getElementById("input-container");
+let texto = document.getElementById("text-container");
+            letra.addEventListener("keydown", function(event) {
+              if(event) {
+                var n = texto.filter(letra);
+            }
+            })
